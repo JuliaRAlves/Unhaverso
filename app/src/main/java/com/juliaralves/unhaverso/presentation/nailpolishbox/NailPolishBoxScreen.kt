da2f97b4f242482aa769157ca90b809bb5ba5431
Julia Rezende Alves <Julia@Alefes-Mac-mini.local>
2025-08-31 16:00:30 -0300

    adding in construction pages

--- app/src/main/java/com/juliaralves/unhaverso/presentation/nailpolishbox/NailPolishBoxScreen.kt
+++ app/src/main/java/com/juliaralves/unhaverso/presentation/nailpolishbox/NailPolishBoxScreen.kt
@@ -1,7 +1,40 @@
 package com.juliaralves.unhaverso.presentation.nailpolishbox
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.juliaralves.unhaverso.R
 
 @Composable
 fun NailPolishBoxScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(R.drawable.bg_building),
+            contentDescription = null,
+            modifier = Modifier.size(400.dp)
+        )
+        Text(
+            text = stringResource(id = R.string.building),
+            modifier = Modifier.padding(vertical = 24.dp),
+            style = MaterialTheme.typography.bodyLarge,
+            color = MaterialTheme.colorScheme.onSurface,
+            textAlign = TextAlign.Center
+        )
+    }
 }
